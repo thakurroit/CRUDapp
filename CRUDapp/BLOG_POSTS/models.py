457c8e065a6bcddf6a19578e213183824dbca274
thakurroit <54647416+thakurroit@users.noreply.github.com>
2020-07-04 14:18:55 -0700

    Added Class CRUD under models and added CRUD functionality using admin portal

--- CRUDapp/BLOG_POSTS/models.py
+++ CRUDapp/BLOG_POSTS/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CRUD(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+
